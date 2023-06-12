57d086431a5aa97030adcdbef0c67a0b0ae414c4
Lucas Ximenes <lucasdximenes@gmail.com>
2023-06-12 17:43:55 -0300

    test: :test_tube: add failing user model unit testing

---
+++ test/mocks/models/user.mock.ts
@@ -0,0 +1,15 @@
+import IUser from "../../../src/interfaces/IUser";
+
+export const userMock: IUser = {
+  id: "e2d3286f-2d8f-471a-bacb-1e5d28d8727e",
+  name: "João Oliveira",
+  job: "Desenvolvedor",
+};
+
+export const usersMock: IUser[] = [
+  {
+    id: "e2d3286f-2d8f-471a-bacb-1e5d28d8727e",
+    name: "João Oliveira",
+    job: "Desenvolvedor",
+  },
+];
---
+++ test/models/user.model.test.ts
@@ -0,0 +1,40 @@
+import * as chai from "chai";
+import * as sinon from "sinon";
+import fs from "fs/promises";
+
+import UserModel from "../../src/models/jsonModel/User.model";
+import { userMock, usersMock } from "../mocks/models/user.mock";
+
+/* 
+  Seguindo uma abordagem de TDD, o teste deve ser escrito antes do código
+  que será testado. Então, o primeiro passo é escrever o teste.
+  O teste deve ser escrito de forma que ele falhe, pois se ele passar
+  sem que o código seja escrito, não há garantia de que o teste está
+  funcionando.
+*/
+
+describe("User Model", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("Should return a user", async () => {
+    sinon.stub(fs, "readFile").resolves(JSON.stringify(usersMock));
+
+    const userModel = new UserModel();
+    const user = await userModel.getById(userMock.id);
+
+    chai.expect(user).to.be.deep.equal(userMock);
+    // Testa se o usuário retornado é igual ao usuário mockado.
+  });
+
+  it("Should return all users", async () => {
+    sinon.stub(fs, "readFile").resolves(JSON.stringify(usersMock));
+
+    const userModel = new UserModel();
+    const users = await userModel.getAll();
+
+    chai.expect(users).to.be.deep.equal(usersMock);
+    // Testa se os usuários retornados são iguais aos usuários mockados.
+  });
+});
